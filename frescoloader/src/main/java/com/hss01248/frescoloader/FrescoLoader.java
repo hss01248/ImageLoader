a0789bef844fb295f2eb2d3505871f53db4e6234
hss01248 <hss012489@gmail.com>
2017-05-01 23:37:46 +0800

    kkk

--- frescoloader/src/main/java/com/hss01248/frescoloader/FrescoLoader.java
+++ frescoloader/src/main/java/com/hss01248/frescoloader/FrescoLoader.java
@@ -325,9 +325,6 @@ public class FrescoLoader implements ILoader {
                         config.getBitmapListener().onSuccess(bitmap);
                         return;
                     }
-
-
-
                 }
 
                 File cacheFile  = getFileFromDiskCache(finalUrl);
