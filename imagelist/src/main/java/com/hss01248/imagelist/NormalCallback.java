478f29a03e81274ed60389c64a94ea87c9f09f06
hss01248 <hss012489@gmail.com>
2021-08-13 17:57:28 +0800

    优化逻辑

--- imagelist/src/main/java/com/hss01248/imagelist/NormalCallback.java
+++ imagelist/src/main/java/com/hss01248/imagelist/NormalCallback.java
@@ -1,5 +1,7 @@
 package com.hss01248.imagelist;
 
+import java.util.List;
+
 /**
  * time:2019/11/30
  * author:hss
@@ -8,6 +10,8 @@ package com.hss01248.imagelist;
 public interface NormalCallback<T> {
 
     void onSuccess(T t, Object extra);
+
+    //void onSuccessEnd(List<T> t, Object extra);
 
     void onFail(Throwable e);
 
