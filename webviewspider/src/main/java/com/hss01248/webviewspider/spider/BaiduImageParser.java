7fb03c0b35b783ce47fa2edb0e24a8d0217c6f5c
hss01248 <hss012489@gmail.com>
2021-11-05 18:02:49 +0800

    百度图片爬取

--- webviewspider/src/main/java/com/hss01248/webviewspider/spider/BaiduImageParser.java
+++ webviewspider/src/main/java/com/hss01248/webviewspider/spider/BaiduImageParser.java
@@ -1,9 +1,15 @@
 package com.hss01248.webviewspider.spider;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.webkit.ValueCallback;
 
+import com.blankj.utilcode.util.LogUtils;
 import com.hss01248.webviewspider.basewebview.WebPageInfo;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +22,32 @@ public class BaiduImageParser implements IHtmlParser{
 
     @Override
     public List<String> parseDetailPage(String html) {
-        return new ArrayList<>();
+        //img.sfc-image-content-ssr-img  src
+        final ArrayList<String> urls = new ArrayList<>();
+        try {
+            Element element0 = Jsoup.parse(html).body();
+            ///img.sfc-image-content-ssr-img  src
+            //https://p3-search.byteimg.com/img/labis/0597604662ce7ba8b1079c49553e0f58~480x480.JPEG
+            //https://p3-search.byteimg.com/img/labis/0597604662ce7ba8b1079c49553e0f58~0x0.JPEG
+            Elements elements = element0.select("div.sfc-image-content-norcell-img-wrapper > div > img");
+            //选择多个class,则不要有空格
+            if(elements == null || elements.isEmpty()){
+                LogUtils.w("baidu image empty!!");
+            }else {
+                for (Element element : elements) {
+                    String url =  element.attr("src");
+                    if(!TextUtils.isEmpty(url)){
+                        LogUtils.d("图 url : ", url);
+                        urls.add(url);
+                    }else {
+                        LogUtils.w("img的 src 属性为空",element);
+                    }
+                }
+            }
+        }catch (Throwable throwable){
+            LogUtils.w(throwable);
+        }
+        return urls;
     }
 
     @Override
