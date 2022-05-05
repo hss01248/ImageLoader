59669010defebb069c74db557b9ded777e67ed82
hss01248 <hss012489@gmail.com>
2022-05-05 14:15:06 +0800

    fresco-avif

---
+++ avif-decoder-demo/src/main/java/com/hss01248/avif_decoder_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hss01248.avif_decoder_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
