d31fab221ad02275da3a6ca6233d2430dee31766
水牛 <hss01248@gmail.com>
2022-06-29 08:15:36 +0800

    feature:下载文件名优化

--- webviewspider/src/main/java/com/hss01248/webviewspider/basewebview/WebviewDownlader.java
+++ webviewspider/src/main/java/com/hss01248/webviewspider/basewebview/WebviewDownlader.java
@@ -2,6 +2,7 @@ package com.hss01248.webviewspider.basewebview;
 
 import android.content.DialogInterface;
 import android.os.Environment;
+import android.text.TextUtils;
 import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
 
@@ -23,15 +24,45 @@ public class WebviewDownlader implements DownloadListener {
     public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
         LogUtils.i(url,userAgent,contentDisposition,mimetype,contentLength);
         String name = URLUtil.guessFileName(url,contentDisposition,mimetype);
-        String str = "是否从\n"+url+"下载文件:\n"+name+"?\n文件大小预计:"+ ConvertUtils.byte2FitMemorySize(contentLength,1);
+        //contentDisposition:  attachment; filename="redditsave.com_p0dqho9nqh891.gif"
+        if(!TextUtils.isEmpty(contentDisposition)){
+            if(contentDisposition.contains(";")){
+                String[] split = contentDisposition.split(";");
+                for (String s : split) {
+                    if(!TextUtils.isEmpty(s)){
+                        s = s.trim();
+                        if(s.contains("=")){
+                            String[] split1 = s.split("=");
+                           if(split1.length == 2){
+                               if("filename".equals(split1[0])){
+                                   String str = split1[1];
+                                   str = str.replace("\"","");
+                                   str = str.replace("\"","");
+                                   str = str.replace("\"","");
+                                   str = str.replace("\"","");
+                                   name = str;
+                                   break ;
+                               }
+                           }
+                        }
+                    }
+                }
+            }
+        }
+        String size = "未知";
+        if(contentLength >0){
+            size = ConvertUtils.byte2FitMemorySize(contentLength,1);
+        }
+        String str = "是否从\n"+url+"下载文件:\n"+name+"?\n文件大小预计:"+ size;
 
+        String finalName = name;
         AlertDialog dialog  = new AlertDialog.Builder(ActivityUtils.getTopActivity())
                 .setTitle("文件下载")
                 .setMessage(str)
                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        doDownload(url,name);
+                        doDownload(url, finalName);
 
                     }
                 }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
