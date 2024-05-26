254acdaaed28a665d6e948a4bcd5a1ea44ed533f
hss01248 <hss012489@gmail.com>
2024-05-26 22:06:10 +0800

    run

--- glidev4/src/main/java/com/hss01248/glidev4/config/GlideModelConfig.java
+++ glidev4/src/main/java/com/hss01248/glidev4/config/GlideModelConfig.java
@@ -1,6 +1,7 @@
 package com.hss01248.glidev4.config;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -9,11 +10,14 @@ import com.blankj.utilcode.util.LogUtils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.LibraryGlideModule;
 import com.hss01248.glide.aop.net.ModifyResponseBodyInterceptor;
 import com.hss01248.image.config.GlobalConfig;
 
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -26,6 +30,8 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import jp.co.link_u.library.glideavif.AvifDecoderFromByteBuffer;
+import me.jessyan.progressmanager.ProgressManager;
 import okhttp3.OkHttpClient;
 
 /**
@@ -44,19 +50,20 @@ public class GlideModelConfig extends LibraryGlideModule {
         LogUtils.w("in lib: registerComponents");
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
                 //.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
-        if(GlobalConfig.debug){
+        //if(GlobalConfig.debug){
             setIgnoreAll(builder);
-        }
+       // }
 
         builder
                 .addNetworkInterceptor(new ModifyResponseBodyInterceptor())
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(30, TimeUnit.SECONDS);
-       // registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(ProgressManager.getInstance()
-      //          .with(builder).build()));
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS);
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(ProgressManager.getInstance()
+                .with(builder).build()));
+        registry.prepend(ByteBuffer.class, Bitmap.class,new AvifDecoderFromByteBuffer());
         //todo 不如直接用aop切,简单粗暴: com.bumptech.glide.load.model.FileLoader.buildLoadData, 将第一个参数File model替换掉
-/*        registry.prepend(ByteBuffer.class, Bitmap.class,new AvifDecoderFromByteBuffer());
+/*
 
         registry.replace(File.class, InputStream.class, new MyStreamFactory());
 
